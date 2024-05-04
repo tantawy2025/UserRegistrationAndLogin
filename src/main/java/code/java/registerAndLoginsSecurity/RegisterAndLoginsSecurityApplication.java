e51c345a5ce11af82b148ab085d666110c9978dd
mohamed tantawy <mtantawy2025@gmail.com>
2024-05-04 04:01:30 +0300

    init project

---
+++ src/main/java/code/java/registerAndLoginsSecurity/RegisterAndLoginsSecurityApplication.java
@@ -0,0 +1,13 @@
+package code.java.registerAndLoginsSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAndLoginsSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAndLoginsSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/java/registerAndLoginsSecurity/RegisterAndLoginsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package code.java.registerAndLoginsSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterAndLoginsSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
